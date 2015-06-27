5230d5e8199846e5b4cde54fc35fbf42bbe36f92
raheelshahzad <raheel.shahzad@gmail.com>
2015-06-26 18:13:17 -0700

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -5,7 +5,7 @@
 using std::unordered_map;
 using std::pair;
 using std::make_pair;
-
+ 
 struct pairhash {
 public:
   template <typename T, typename U>
@@ -29,14 +29,26 @@ class Matrix {
 		void swap(Matrix& other) {
 			map_.swap(other.map_);
 		}
-		bool setVal(NodeKeyType i, NodeKeyType j, ValueType val) {
-			pair<NodeKeyType, NodeKeyType> p = std::make_pair(i, j);
-			map_.erase(p);
-			map_.insert(table::value_type(std::make_pair(p, val)));
+		bool setValue(NodeKeyType i, NodeKeyType j, ValueType val) {
+			pair<NodeKeyType, NodeKeyType> key = std::make_pair(i, j);
+			return setValue(key, val);
+		}
+
+		bool setValue(const pair<NodeKeyType, NodeKeyType> &key, ValueType val) {
+			map_.erase(key);
+			return map_.insert(table::value_type(key, val));
+		}
+
+		ValueType getValue(const pair<NodeKeyType, NodeKeyType> &key) {
+			pair<NodeKeyType, NodeKeyType> key = std::make_pair(i, j);
+			table::const_iterator itt = map_.find(key);
+
+			return itt != map_.end() ? itt->second : 0.0;
 		}
 		void clear() {
 			map_.clear();
 		}
+		void weightedAdd(double weight, const Matrix& ohter);
 		double eucledeanDistance(const Matrix &other_) const;
 		double eucledeanDistance() const;
 
