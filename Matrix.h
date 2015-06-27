aa6048775527de18ab7846d1cb020f9052f4d8f4
raheelshahzad <raheel.shahzad@gmail.com>
2015-06-26 17:13:08 -0700

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,37 +1,45 @@
-typedef unsigned long long ValueType;
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+
+using std::unordered_map;
+using std::pair;
+using std::make_pair;
+
+struct pairhash {
+public:
+  template <typename T, typename U>
+  std::size_t operator()(const std::pair<T, U> &x) const
+  {
+    return std::hash<T>()(x.first) ^ std::hash<U>()(x.second);
+  }
+};
+
+
+typedef int NodeKeyType;
+typedef double ValueType;
+
+typedef unordered_map<pair<NodeKeyType, NodeKeyType>, ValueType, pairhash> table;
+
 class Matrix {
-	private:
-		int n_;
-		int m_;
-		ValueType **matrix_;
-		Matrix() {}
-		void resize(int n, int m);
-		void clear();
-		void deepCopy(const Matrix& other);
-		void init(int n, int m);
 	public:
-		Matrix(int n, int m) {
-			init(n, m);
-		}
-		Matrix(int n) {
-			init(n, n);
-		}
-		Matrix(const Matrix& other) : n_(0), m_(0), matrix_(0) {
-			deepCopy(other);
-		}
-		Matrix& operator = (const Matrix& other) {
-			deepCopy(other);
-			return *this;
+		Matrix(const Matrix& other) : map_(other.map_) {
 		}
 
-		void swap(Matrix& other);
-		bool setVal(int i, int j, ValueType val);
-		double eucledeanDistance(const Matrix &other_);
-		double eucledeanDistance();
-		~Matrix() {
-			for (int i = 0; i < n_; i++) {
-				delete []matrix_[i];
-			}
-			delete []matrix_;
+		void swap(Matrix& other) {
+			map_.swap(other.map_);
 		}
+		bool setVal(NodeKeyType i, NodeKeyType j, ValueType val) {
+			pair<NodeKeyType, NodeKeyType> p = std::make_pair(i, j);
+			map_.erase(p);
+			map_.insert(table::value_type(std::make_pair(p, val)));
+		}
+		void clear() {
+			map_.clear();
+		}
+		double eucledeanDistance(const Matrix &other_) const;
+		double eucledeanDistance() const;
+
+	private:
+		table map_;
 };
